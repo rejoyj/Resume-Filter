9273f3525926fbef5260bc2d32ad0ef4666179c5
Rejoy J <rrejoy7@gmail.com>
2025-08-05 22:14:10 +0530

    DDD

--- Resume-Filter/vite.config.js
+++ Resume-Filter/vite.config.js
@@ -1,8 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Resume-Filter/', // 👈 Add this line
+  base: '/Resume-Filter/',
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      output: {
+        // Create 404.html as a copy of index.html
+        entryFileNames: assetInfo => assetInfo.name === '404' ? '404.html' : 'assets/[name]-[hash].js',
+      }
+    }
+  }
+});
