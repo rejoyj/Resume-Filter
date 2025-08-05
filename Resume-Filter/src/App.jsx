05804f2ec594ca1ca359287bceab7a28de13e032
Rejoy J <rrejoy7@gmail.com>
2025-08-05 22:01:54 +0530

    Error Updated

--- Resume-Filter/src/App.jsx
+++ Resume-Filter/src/App.jsx
@@ -1,18 +1,20 @@
 import { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import { BrowserRouter } from 'react-router-dom';
+
 
 import Page from './components/Page.jsx';
 import SideBar from './components/SideBar.jsx';
 import Homepage from './components/Homepage.jsx';
 import MailTemplatePage from './components/MailTemplatePage.jsx';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import {Routes, Route } from 'react-router-dom';
 import JobDescription from './components/JobDescription.jsx';
 import Chatbot from './components/Chatbot.jsx'
 import BroadCast from './components/BroadCast.jsx'
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/Resume-Filter">
       <div className="d-flex">
         {/* Sidebar visible on all pages */}
         <SideBar />
