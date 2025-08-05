8751020f04585cc2893a12782dd906f4547231b6
Rejoy J <rrejoy7@gmail.com>
2025-08-05 23:09:14 +0530

    BBB

--- Resume-Filter/vite.config.js
+++ Resume-Filter/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Resume-Filter/',  // 👈 ADD THIS LINE
+  base: '/Chatbot/', // 👈 this must match your repo name
   plugins: [react()],
 })
